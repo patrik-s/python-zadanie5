9075c3dfe900c6b8c449c62a68244168bb0a65eb
patrik-s <serdula.p@gmail.com>
2021-09-20 10:09:43 +0200

    prvy komentar

---
+++ s.py
@@ -0,0 +1,28 @@
+
+meno = input("Meno hraca: ")
+print("Okey {0}\nPravidla su nasledovne. Ja si myslim cislo a ty budes hadat.\nAk chces ukoncit hru, napis 'KONIEC'.\nNa konci hry uvidis svoje skore.".format(meno))
+
+i = 0
+j = 0
+g = 'stoj'
+while g != 'koniec':
+	print("\n")
+	print("Myslim si cislo: ")
+	while g != 'koniec':
+		import random
+		random = random.randint(1,10)
+		g = input("Tvoj tip?: ")
+		if g != "koniec":
+			if g == str(random):
+				print("Spravne")
+				i += 1
+				break
+			else :
+				print("Nespravne")
+				j += 1
+vysledok = """+----tvoje skore-----+
+| spravne   |   {}    |
+| nespravne |   {}   |
+| celkom    |   {}   |
++====================+"""  				
+print(vysledok.format(i,j,i+j))
